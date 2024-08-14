35e649f2b18e78411e2e669a17083e015d900793
liuyihua <liuyihua@shinsson.com>
2024-08-14 17:46:41 +0800

    feat: 会议室列表 搜索 删除等功能

--- src/interface/interfaces.ts
+++ src/interface/interfaces.ts
@@ -108,3 +108,25 @@ export async function updatePasswordCaptcha(email: string) {
 export async function updatePassword(data: UpdatePassword) {
   return await axiosInstance.post("/user/admin/update_password", data);
 }
+
+export async function meetingRoomList(
+  name: string,
+  capacity: number,
+  equipment: string,
+  pageNo: number,
+  pageSize: number
+) {
+  return await axiosInstance.get("/meeting-room/list", {
+    params: {
+      name,
+      capacity,
+      equipment,
+      pageNo,
+      pageSize,
+    },
+  });
+}
+
+export async function deleteMeetingRoom(id: number) {
+  return await axiosInstance.delete("/meeting-room/" + id);
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,34 +1,67 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Menu as AntdMenu, MenuProps } from "antd";
+import { router } from "../.."; // 确保router被正确引入
 import "./menu.css";
+import { MenuClickEventHandler } from "rc-menu/lib/interface";
 
-const items: MenuProps["items"] = [
-  {
-    key: "1",
-    label: "会议室管理",
-  },
-  {
-    key: "2",
-    label: "预定管理",
-  },
-  {
-    key: "3",
-    label: "用户管理",
-  },
-  {
-    key: "4",
-    label: "统计",
-  },
-];
+const pathToKeyMapping = {
+  "/meeting_room_manage": "1",
+  "/booking_manage": "2",
+  "/user_manage": "3",
+  "/statistics": "4",
+};
+
+const handleMenuItemClick: MenuClickEventHandler = (info) => {
+  const path = Object.keys(pathToKeyMapping).find(
+    (key) => pathToKeyMapping[key as keyof typeof pathToKeyMapping] === info.key
+  );
+  if (path) {
+    router.navigate(path);
+  }
+};
 
 export function Menu() {
+  const location = useLocation();
+  function getSelectedKeys() {
+    if (location.pathname in pathToKeyMapping) {
+      return [
+        pathToKeyMapping[location.pathname as keyof typeof pathToKeyMapping],
+      ];
+    } else {
+      return ["1"]; // You can choose to return other default values or an empty array
+    }
+  }
+
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: "会议室管理",
+    },
+    {
+      key: "2",
+      label: "预定管理",
+    },
+    {
+      key: "3",
+      label: "用户管理",
+    },
+    {
+      key: "4",
+      label: "统计",
+    },
+  ];
+
   return (
     <div id="menu-container">
       <div className="menu-area">
-        <AntdMenu defaultSelectedKeys={["3"]} items={items} />
+        <AntdMenu
+          defaultSelectedKeys={getSelectedKeys()}
+          items={items}
+          onClick={handleMenuItemClick}
+        />
       </div>
       <div className="content-area">
-        <Outlet></Outlet>
+        <Outlet />
       </div>
     </div>
   );
